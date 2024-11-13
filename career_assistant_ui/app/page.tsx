1fb3bb9c4ae9b5ea2f3c6065118b013b53c4a055
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-13 14:56:11 +0530

    ui fixes

--- career_assistant_ui/app/page.tsx
+++ career_assistant_ui/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState, useEffect } from 'react'
 import { Device } from '@twilio/voice-sdk';
+import axios from 'axios';
 
 enum Codec {
   Opus = "opus",
@@ -13,9 +14,21 @@ export default function Home() {
 
   useEffect(() => {
     const initializeDevice = async () => {
-      const response = await fetch('/api/token', { method: 'GET' });
-      const data = await response.json();
-      setToken(data.token);
+
+      axios(process.env.NEXT_PUBLIC_API_URL + '/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true,
+        data: {
+          identity: 'user'
+        }
+      }).then((response) => {
+        setToken(response.data.token);
+      }).catch((err) => {
+        console.log("err", err)
+      })
 
     };
 
@@ -23,6 +36,7 @@ export default function Home() {
   }, []);
 
   const startCall = () => {
+    console.log('startCall');
     if (!token) return;
 
     setCallInProgress(true);
@@ -51,12 +65,17 @@ export default function Home() {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      {!callInProgress ? (
-        <button onClick={startCall}>Start</button>
-      ) : (
-        <p>Call in progress...</p>
-      )}
+    <div className='w-screen h-screen flex flex-col items-center justify-center bg-white/80'>
+      <div className=' flex flex-col items-center bg-gray-100 p-8 rounded-xl w-4/12'>
+        <button className=' cursor-pointer text-white bg-green-600 px-4 py-2 rounded-xl mb-2' onClick={startCall}>Start</button>
+        {
+          callInProgress ? <p className=' text-black'>Call in Progress</p> : <p className=' text-black'>No Call</p>
+        }
+
+        {
+          token && <p className=' text-green-500 mt-4'>Ready</p>
+        }
+      </div>
     </div>
   );
 }
