4a76c456fb8ad670e68e480292cb304b1f989785
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-16 21:47:34 +0530

    config changes

--- career_assistant_be/src/config/awsConfig.js
+++ career_assistant_be/src/config/awsConfig.js
@@ -1,4 +1,6 @@
 const { S3Client } = require("@aws-sdk/client-s3");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
 exports.s3Client = new S3Client({
   region: process.env.AWS_REGION_1,
@@ -7,3 +9,13 @@ exports.s3Client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
+
+const dynamoClient = new DynamoDBClient({
+  region: process.env.AWS_REGION_2,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_1_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+exports.dynamoDBDocumentClient = DynamoDBDocumentClient.from(dynamoClient);
