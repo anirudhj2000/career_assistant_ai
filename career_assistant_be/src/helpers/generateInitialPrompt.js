a5018915af260fe7d4f8d285e20c698c3c843aca
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-17 14:14:14 +0530

    generate initial prompt

---
+++ career_assistant_be/src/helpers/generateInitialPrompt.js
@@ -0,0 +1,33 @@
+const {
+  SYSTEM_MESSAGE_EXISTING_USER,
+  SYSTEM_MESSAGE_EXISTING_USER_NO_RESUME,
+  SYSTEM_MESSAGE_NEW_USER,
+} = require("../utils/consts");
+const { getUserData } = require("../controller/controller");
+const { wsEvents, isWsCallReady } = require("../utils/readyEvent");
+const { userTypes } = require("../utils/consts");
+const { generateResumeSummary } = require("./manageConversation");
+
+exports.generateInitialPrompt = async () => {
+  let response = await getUserData(isWsCallReady.user);
+
+  console.log("User data response prompt: ", response, isWsCallReady.userType);
+
+  let resumeSummary = "";
+  if (isWsCallReady.userType == userTypes.NEW_USER) {
+    return SYSTEM_MESSAGE_NEW_USER;
+  } else if (
+    isWsCallReady.userType == userTypes.EXISTING_USER &&
+    response.resume
+  ) {
+    resumeSummary = await generateResumeSummary(response.Item.resume);
+    return SYSTEM_MESSAGE_EXISTING_USER + resumeSummary;
+  } else if (
+    isWsCallReady.userType === userTypes.EXISTING_USER_NO_RESUME ||
+    !response.resume ||
+    !response.name
+  ) {
+    resumeSummary = await generateResumeSummary(response.Item.resume);
+    return SYSTEM_MESSAGE_EXISTING_USER_NO_RESUME + resumeSummary;
+  }
+};
