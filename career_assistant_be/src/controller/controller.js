b12529e8698441f1689a0891ca0f4353f35a3ec6
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-17 07:07:09 +0530

    routes and controller changes

--- career_assistant_be/src/controller/controller.js
+++ career_assistant_be/src/controller/controller.js
@@ -1,11 +1,35 @@
 const { VoiceResponse } = require("twilio").twiml;
-const { generateApiToken } = require("../utils");
+const { generateApiToken } = require("../utils/token");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const { PutObjectCommand } = require("@aws-sdk/client-s3");
 const { s3Client } = require("../config/awsConfig");
 const { scrapeJobs } = require("../utils/scraper");
+const { resume_dummy } = require("../utils/consts");
+const { generatePDF } = require("../helpers/generatePdf");
+
+exports.createUser = async (req, res) => {
+  try {
+    const { identity } = req.body;
+    const token = generateApiToken(identity);
+    res.status(200).send({ token, identity });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+exports.handleIncomingCall = (req, res) => {
+  console.log("Incoming call from:", req.query.From, req.headers.host);
+  const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
+                          <Response>
+                              <Connect>
+                                  <Stream url="wss://${req.headers.host}/media-stream" />
+                              </Connect>
+                          </Response>`;
+  res.type("text/xml").send(twimlResponse);
+};
 
 exports.generateToken = async (req, res) => {
   try {
@@ -23,4 +47,15 @@ exports.getJobs = async (req, res) => {
 
   const jobs = await scrapeJobs(type);
   res.status(200).send(jobs);
+};
+
+exports.generateResumePdf = async (req, res) => {
+  try {
+    const { resume } = req.body;
+    const pdf = await generatePDF(resume_dummy);
+    res.status(200).send(pdf);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
--- career_assistant_be/src/routes.js
+++ career_assistant_be/src/routes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const controller = require("./controller/controller");
 
 router.post("/token", controller.generateToken);
-router.get("/voice-response", controller.voiceResponse);
-router.post("/handle-response", controller.handleResponse);
-router.post("/save-recording", controller.saveRecording);
 router.get("/jobs", controller.getJobs);
+router.post("/create-user", controller.createUser);
 router.get("/incoming-call", controller.handleIncomingCall);
+router.post("/generate-pdf", controller.generateResumePdf);
 
 module.exports = router;
