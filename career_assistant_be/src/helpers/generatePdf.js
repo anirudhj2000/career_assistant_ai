44c2c73d3782b03c29d1c75d4ca342e4dc28ae92
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-17 07:09:09 +0530

    generate pdf changes

---
+++ career_assistant_be/src/helpers/generatePdf.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const ejs = require("ejs");
+const puppeteer = require("puppeteer");
+
+exports.generatePDF = async (jsonData) => {
+  try {
+    // Render the EJS template
+    const template = fs.readFileSync(
+      __dirname + "/../templates/resume-template.ejs",
+      "utf8"
+    );
+    const htmlContent = ejs.render(template, jsonData);
+
+    // Launch Puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Set content and generate PDF
+    await page.setContent(htmlContent);
+    await page.pdf({ path: "resume.pdf", format: "A4", printBackground: true });
+
+    console.log("PDF generated successfully!");
+    await browser.close();
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+  }
+};
